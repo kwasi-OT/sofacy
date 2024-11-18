059a0087d51483e42f224d1a93fcb412ca01fc82
Kwasi Ofori-Tweneboah <tweneboah21@gmail.com>
2024-11-18 00:06:30 +0000

    added functions to add/remove items from cart, plus styling

--- cart.js
+++ cart.js
@@ -20,42 +20,57 @@ const cart = () => {
                     product_id: productId,
                     quantity: quantity
                 });
+            } else {
+                cart[position].quantity = quantity;
             }
         } else {
-            cart[position].quantity = quantity;
+            cart.splice(position, 1);
         }
-        iconCart.querySelector('span').innerHTML = cart.length;
         renderCart();
     }
 
     const renderCart = () => {
         let listCart = document.querySelector('.listCart');
+        let totalHTML = document.querySelector('.cart-icon span');
+        let totalQuantity = 0;
         listCart.innerHTML = null;
         cart.forEach(item => {
-            let product = products.find(product => product.id == item.product_id);
+            totalQuantity = totalQuantity + item.quantity;
+            let product = products.findIndex(product => product.id == item.product_id);
+            let info = products[product];
             let productItem = document.createElement('div');
-            productItem.classList.add('product-item');
+            productItem.classList.add('item');
             productItem.innerHTML = `
-                <img src="${product.image}" alt="${product.name}">
-                <h3>${product.name}</h3>
-                <p class="price">GHC ${product.price}</p>
-                <p class="quantity">Quantity: ${item.quantity}</p>
+                <div class='image'>
+                    <img src="${info.image}" alt="${info.name}">
+                </div>
+                <div class="name">${info.name}</div>
+                <div class="totalPrice">GHC ${info.price * item.quantity}</div>
+                <div class="quantity">
+                    <span class="decrease" data-id="${info.id}">-</span>
+                    <span>${item.quantity}</span>
+                    <span class="increase" data-id="${info.id}">+</span>
+                </div>
             `;
             listCart.appendChild(productItem);
         });
+        totalHTML.innerHTML = totalQuantity;
     }
 
     // button click event
     document.addEventListener('click', (event) => {
         let buttonClick = event.target;
         let productId = buttonClick.dataset.id;
-        let position = cart.findIndex(item => item.id == productId);
+        let position = cart.findIndex((item) => item.product_id == productId);
         let quantity = position < 0 ? 0 : cart[position].quantity;
 
-        if (buttonClick.classList.contains('addToCart')) {
+        if (buttonClick.classList.contains('addToCart') || buttonClick.classList.contains('increase')) {
             quantity++;
             setProductInCart(productId, quantity, position);
-        }
+        } else if (buttonClick.classList.contains('decrease')) {
+            quantity--;
+            setProductInCart(productId, quantity, position);
+        } 
     });
 }
 
